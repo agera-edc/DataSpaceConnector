402da9404f2ebbed1b8cfd6e7bb6b970c1d27b90
Ophelie Le Mentec <olementec@microsoft.com>
2022-02-16 13:28:57 +0100

    Pass config.properties file with blob name and other configs.

--- blobspike/src/main/java/BlobStorageManager.java
+++ blobspike/src/main/java/BlobStorageManager.java
@@ -1,19 +1,41 @@
-import com.azure.storage.blob.*;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
+import java.util.Properties;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BlobStorageManager {
 
     public static void main(String[] args) {
-        String connectionString = ""; //connectionString
-        BlobInfo sourceBlob = new BlobInfo("hello.txt", "container1", connectionString);
-        BlobInfo destBlob = new BlobInfo("hello2.txt", "container2", connectionString);
-        copyBlob(sourceBlob, destBlob);
+        String propertiesFilePath = args[0];
+        try (InputStream input = new FileInputStream(propertiesFilePath)) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            String sourceConnectionString = prop.getProperty("source.connection.string");
+            String destConnectionString = prop.getProperty("dest.connection.string");
+            String sourceContainer = prop.getProperty("source.container.name");
+            String destContainer = prop.getProperty("dest.container.name");
+            String sourceBlobName = prop.getProperty("source.blob.name");
+            String destBlobName = prop.getProperty("dest.blob.name");
+
+            BlobInfo sourceBlob = new BlobInfo(sourceBlobName, sourceContainer, sourceConnectionString);
+            BlobInfo destBlob = new BlobInfo(destBlobName, destContainer, destConnectionString);
+            new BlobStorageManager().copyBlob(sourceBlob, destBlob);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void copyBlob(BlobInfo sourceBlob, BlobInfo destBlob) {
+    public void copyBlob(BlobInfo sourceBlob, BlobInfo destBlob) {
         BlobContainerClient sourceContainerClient = new BlobContainerClientBuilder()
                 .connectionString(sourceBlob.storageAccountConnectionString)
                 .containerName(sourceBlob.containerName)
@@ -27,5 +49,11 @@ public class BlobStorageManager {
         BlobClient destBlobClient = destContainerClient.getBlobClient(destBlob.blobName);
         String source = sourceBlobClient.getBlobUrl();
         destBlobClient.beginCopy(source, Duration.ofSeconds(1L));
+
+        var sourceBlobContent = new ByteArrayOutputStream();
+        var destBlobContent = new ByteArrayOutputStream();
+        sourceBlobClient.download(sourceBlobContent);
+        destBlobClient.download(destBlobContent);
+        assertEquals(sourceBlobContent.toString(), destBlobContent.toString());
     }
 }
