6ebe5ba74609298450dd917bbbb4da47575f1804
Alexandre Gattiker <algattik@microsoft.com>
2022-02-21 13:34:25 +0100

    Update BlobAdapterFactoryTest.java

--- extensions/azure/data-plane-azure-storage/src/test/java/org/eclipse/dataspaceconnector/azure/dataplane/azurestorage/adapter/BlobAdapterFactoryTest.java
+++ extensions/azure/data-plane-azure-storage/src/test/java/org/eclipse/dataspaceconnector/azure/dataplane/azurestorage/adapter/BlobAdapterFactoryTest.java
@@ -2,6 +2,7 @@ package org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.adapter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.pipeline.AzureStorageTestFixtures.createAccountName;
 import static org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.pipeline.AzureStorageTestFixtures.createBlobName;
 import static org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.pipeline.AzureStorageTestFixtures.createContainerName;
@@ -12,10 +13,11 @@ class BlobAdapterFactoryTest {
     @Test
     void getBlobAdapter_succeeds() {
         BlobAdapterFactory f = new BlobAdapterFactory(null);
-        f.getBlobAdapter(
-                createAccountName(),
-                createContainerName(),
-                createBlobName(),
-                createSharedKey());
+        assertThatNoException()
+                .isThrownBy(() -> f.getBlobAdapter(
+                                createAccountName(),
+                                createContainerName(),
+                                createBlobName(),
+                                createSharedKey()));
     }
 }
