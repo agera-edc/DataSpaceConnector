dcae76b4ac88c65794f79067ea14a88a9603dac2
Alexandre Gattiker <algattik@microsoft.com>
2022-02-22 22:26:16 +0100

    Adapted to use Windows agent in CI

--- extensions/azure/azure-test/src/testFixtures/java/org/eclipse/dataspaceconnector/azure/testfixtures/CosmosTestClient.java
+++ extensions/azure/azure-test/src/testFixtures/java/org/eclipse/dataspaceconnector/azure/testfixtures/CosmosTestClient.java
@@ -11,11 +11,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.security.KeyStore;
 import java.security.SecureRandom;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -30,13 +28,13 @@ public interface CosmosTestClient {
     static CosmosClient createClient() {
         var cosmosKey = propOrEnv("COSMOS_KEY", null);
         if (cosmosKey != null) {
-            return clientForCi(cosmosKey);
+            return azureClient(cosmosKey);
         } else {
             return localClient();
         }
     }
 
-    private static CosmosClient clientForCi(String cosmosKey) {
+    private static CosmosClient azureClient(String cosmosKey) {
         return new CosmosClientBuilder()
                 .key(cosmosKey)
                 .preferredRegions(List.of("westeurope"))
@@ -48,26 +46,9 @@ public interface CosmosTestClient {
         try {
             var endpoint = "https://127.0.0.1:8081";
 
-            var keystorePath = System.getProperty("java.home") + separator + "lib" + separator + "security" + separator + "cacerts";
-            var keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustCertificateFrom(endpoint);
 
-            var keystorePassword = "changeit".toCharArray();
-            keystore.load(new FileInputStream(keystorePath), keystorePassword);
-
-            var certificate = getCertificateFrom(endpoint);
-            keystore.setCertificateEntry("cosmosdb", certificate);
-
-            try (var output = new FileOutputStream(keystorePath)) {
-                keystore.store(output, keystorePassword);
-            }
-
-            var newKeystorePath = Files.createTempFile(null, null);
-            try (var output = new FileOutputStream(newKeystorePath.toFile())) {
-                keystore.store(output, keystorePassword);
-            }
-            System.setProperty("javax.net.ssl.trustStore", newKeystorePath.toString());
-
-            // Seems to be a fixed value: https://github.com/Azure/azure-cosmos-db-emulator-docker
+            // Emulator key is a fixed value: https://github.com/Azure/azure-cosmos-db-emulator-docker
             String masterKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
             return new CosmosClientBuilder()
@@ -80,19 +61,18 @@ public interface CosmosTestClient {
         }
     }
 
-    private static Certificate getCertificateFrom(String endpoint) {
-        var url = endpoint + "/_explorer/emulator.pem";
-        var client = trustAllHttpClient();
-        var request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        try (var stream = client.send(request, HttpResponse.BodyHandlers.ofInputStream()).body()) {
-            return CertificateFactory.getInstance("X.509").generateCertificate(stream);
+    private static void trustCertificateFrom(String endpoint) {
+        var client = trustStorePopulatingHttpClient();
+        var request = HttpRequest.newBuilder().uri(URI.create(endpoint)).build();
+        try {
+            client.send(request, HttpResponse.BodyHandlers.ofInputStream()).body().close();
         } catch (Exception e) {
-            throw new RuntimeException(format("Error getting certificate. Url: %s", url), e);
+            throw new EdcException(format("Error getting certificate. Url: %s", endpoint), e);
         }
     }
 
-    private static HttpClient trustAllHttpClient() {
-        var trustAllCerts = new TrustManager[]{
+    private static HttpClient trustStorePopulatingHttpClient() {
+        var trustStorePopulator = new TrustManager[]{
             new X509TrustManager() {
                 public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                     return null;
@@ -102,16 +82,43 @@ public interface CosmosTestClient {
                 }
 
                 public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    setUpTrustStoreWithCertificates(certs);
                 }
             }
         };
 
         try {
             SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustAllCerts, new SecureRandom());
+            sslContext.init(null, trustStorePopulator, new SecureRandom());
             return HttpClient.newBuilder().sslContext(sslContext).build();
         } catch (Exception e) {
             throw new EdcException("Error initializing ssl context", e);
+        }
+    }
+
+    private static void setUpTrustStoreWithCertificates(X509Certificate[] certs) {
+
+        try {
+            var trustStorePath =
+                    System.getProperty("javax.net.ssl.trustStore",
+                    System.getProperty("java.home") + separator + "lib" + separator + "security" + separator + "cacerts");
+            var trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword", "changeit").toCharArray();
+
+            var trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(new FileInputStream(trustStorePath), trustStorePassword);
+
+            var certCounter = 0;
+            for (var cert : certs) {
+                trustStore.setCertificateEntry("cosmosdb" + certCounter++, cert);
+            }
+
+            var newTrustStorePath = Files.createTempFile(null, null);
+            try (var output = new FileOutputStream(newTrustStorePath.toFile())) {
+                trustStore.store(output, trustStorePassword);
+            }
+            System.setProperty("javax.net.ssl.trustStore", newTrustStorePath.toString());
+        } catch (Exception e) {
+            throw new EdcException("Error initializing certificate store", e);
         }
     }
 
