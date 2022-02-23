62c81eafa3c74f3c9fd6e277fe4ea07c1215dd84
Ophelie Le Mentec <olementec@microsoft.com>
2022-02-23 13:45:04 +0100

    Added copy by blocks.

--- blobspike/src/main/java/BlobStorageManager.java
+++ blobspike/src/main/java/BlobStorageManager.java
@@ -1,7 +1,11 @@
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.models.BlobRange;
+import com.azure.storage.blob.models.Block;
+import com.azure.storage.blob.models.BlockListType;
 import com.azure.storage.blob.sas.BlobSasPermission;
 import com.azure.storage.blob.sas.BlobServiceSasSignatureValues;
+import com.azure.storage.blob.specialized.BlockBlobClient;
 import io.opentelemetry.extension.annotations.WithSpan;
 
 import java.io.FileInputStream;
@@ -10,6 +14,7 @@ import java.io.InputStream;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 
 public class BlobStorageManager {
@@ -31,6 +36,8 @@ public class BlobStorageManager {
             BlobInfo sourceBlob = new BlobInfo(sourceBlobName, sourceContainer, sourceConnectionString);
             BlobInfo destBlob = new BlobInfo(destBlobName, destContainer, destConnectionString);
             new BlobStorageManager().copyBlobUsingSasToken(sourceBlob, destBlob);
+            // new BlobStorageManager().copyByBlock(sourceBlob, destBlob);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -56,7 +63,6 @@ public class BlobStorageManager {
         String sasToken = sourceBlobClient.generateSas(sas);
 
         String sourceBlobUrl = sourceBlobClient.getBlobUrl() + "?" + sasToken;
-        System.out.println(sourceBlobUrl);
 
         // destination blob client
         BlobClient destBlobClient = new BlobClientBuilder()
@@ -69,6 +75,41 @@ public class BlobStorageManager {
         var syncPoller = destBlobClient.beginCopy(sourceBlobUrl, Duration.ofSeconds(1L));
         // wait for the copy to finish. It will check the blob properties to make sure the copy is finished.
         syncPoller.waitForCompletion();
+    }
+
+    @WithSpan("copy by blocks")
+    public void copyByBlock(BlobInfo sourceBlob, BlobInfo destBlob) {
+        // source blob client
+        BlobClient sourceBlobClient = new BlobClientBuilder()
+                .connectionString(sourceBlob.storageAccountConnectionString)
+                .containerName(sourceBlob.containerName)
+                .blobName(sourceBlob.blobName)
+                .buildClient();
+
+        // set permissions for the blob
+        BlobSasPermission permission = new BlobSasPermission()
+                .setReadPermission(true);
+        // define rule how long the permission is valid
+        BlobServiceSasSignatureValues sas = new BlobServiceSasSignatureValues(OffsetDateTime.now().plusDays(1), permission)
+                .setStartTime(OffsetDateTime.now());
+        // generate sas token
+        String sasToken = sourceBlobClient.generateSas(sas);
+
+        String sourceBlobUrl = sourceBlobClient.getBlobUrl() + "?" + sasToken;
+
+        // destination blob client
+        BlockBlobClient destBlobClient = new BlobClientBuilder()
+                .connectionString(destBlob.storageAccountConnectionString)
+                .containerName(destBlob.containerName)
+                .blobName(destBlob.blobName)
+                .buildClient().getBlockBlobClient();
+
+        var blockIds = sourceBlobClient.getBlockBlobClient().listBlocks(BlockListType.COMMITTED).getCommittedBlocks()
+                .parallelStream().map(Block::getName).collect(Collectors.toList());
+
+        blockIds.forEach(block ->  destBlobClient.stageBlockFromUrl(block, sourceBlobUrl, new BlobRange(0)));
+
+        destBlobClient.commitBlockList(blockIds);
     }
 
 }
