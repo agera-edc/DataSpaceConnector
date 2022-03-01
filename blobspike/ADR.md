62291890a285ed819292d948f4c3eb4ff59eedcf
Ophélie Le Mentec <olementec@microsoft.com>
2022-03-01 10:22:00 +0100

    [Spike] Server side copy of blob - extended evaluation (#110)

---
+++ blobspike/ADR.md
@@ -0,0 +1,28 @@
+# Server side copy extension
+
+## Copy with Java SDK
+
+The optimal strategy to copy blobs on server side is to copy the blob by blocks in parallel.
+The size of the block is configurable. The parallelization is not handled by the client but should be handled the logic on top of it.
+In order to achieve that, call [stageBlockFromUrl](https://docs.microsoft.com/en-us/java/api/com.azure.storage.blob.specialized.blockblobclient.stageblockfromurl?view=azure-java-stable) for each block to copy.
+Once blocks are successfully written, call [commitBlockList](https://docs.microsoft.com/en-us/java/api/com.azure.storage.blob.specialized.blockblobclient.commitblocklist?view=azure-java-stable).
+This will create the blob composed of the blocks.
+Note that a Java data movement library is in design process now. This solution is proposed because the library is not available now.
+
+For example:
+```java
+var sourceSize = sourceBlobClient.getProperties().getBlobSize();
+ArrayList<String> blockIds = new ArrayList<>();
+
+var blockSize = 8 * 1024 * 1024;
+
+var blobRanges = buildRanges(sourceSize, blockSize);
+blobRanges.parallelStream()
+        .forEach(range -> {
+            // BlockId of the destination block
+            var blockId = Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8));
+            destBlobClient.stageBlockFromUrl(blockId, sourceBlobUrl, range);
+        });
+
+destBlobClient.commitBlockList(blockIds);
+```
--- blobspike/src/main/java/BlobStorageManager.java
+++ blobspike/src/main/java/BlobStorageManager.java
@@ -1,16 +1,22 @@
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.models.BlobRange;
 import com.azure.storage.blob.sas.BlobSasPermission;
 import com.azure.storage.blob.sas.BlobServiceSasSignatureValues;
+import com.azure.storage.blob.specialized.BlockBlobClient;
 import io.opentelemetry.extension.annotations.WithSpan;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 import java.util.Properties;
-
+import java.util.UUID;
 
 public class BlobStorageManager {
 
@@ -30,7 +36,9 @@ public class BlobStorageManager {
 
             BlobInfo sourceBlob = new BlobInfo(sourceBlobName, sourceContainer, sourceConnectionString);
             BlobInfo destBlob = new BlobInfo(destBlobName, destContainer, destConnectionString);
-            new BlobStorageManager().copyBlobUsingSasToken(sourceBlob, destBlob);
+            // new BlobStorageManager().copyBlobUsingSasToken(sourceBlob, destBlob);
+            new BlobStorageManager().copyByBlock(sourceBlob, destBlob);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -56,7 +64,6 @@ public class BlobStorageManager {
         String sasToken = sourceBlobClient.generateSas(sas);
 
         String sourceBlobUrl = sourceBlobClient.getBlobUrl() + "?" + sasToken;
-        System.out.println(sourceBlobUrl);
 
         // destination blob client
         BlobClient destBlobClient = new BlobClientBuilder()
@@ -71,5 +78,66 @@ public class BlobStorageManager {
         syncPoller.waitForCompletion();
     }
 
+    @WithSpan("copy by blocks")
+    public void copyByBlock(BlobInfo sourceBlob, BlobInfo destBlob) {
+        System.out.println("Start " + java.time.LocalDateTime.now());
+        // source blob client
+        BlobClient sourceBlobClient = new BlobClientBuilder()
+                .connectionString(sourceBlob.storageAccountConnectionString)
+                .containerName(sourceBlob.containerName)
+                .blobName(sourceBlob.blobName)
+                .buildClient();
+
+        // set permissions for the blob
+        BlobSasPermission permission = new BlobSasPermission()
+                .setReadPermission(true);
+        // define rule how long the permission is valid
+        BlobServiceSasSignatureValues sas = new BlobServiceSasSignatureValues(OffsetDateTime.now().plusDays(1), permission)
+                .setStartTime(OffsetDateTime.now());
+        // generate sas token
+        String sasToken = sourceBlobClient.generateSas(sas);
+
+        String sourceBlobUrl = sourceBlobClient.getBlobUrl() + "?" + sasToken;
+
+        // destination blob client
+        BlockBlobClient destBlobClient = new BlobClientBuilder()
+                .connectionString(destBlob.storageAccountConnectionString)
+                .containerName(destBlob.containerName)
+                .blobName(destBlob.blobName)
+                .buildClient()
+                .getBlockBlobClient();
+
+        var sourceSize = sourceBlobClient.getProperties().getBlobSize();
+        ArrayList<String> blockIds = new ArrayList<>();
+
+        // this size is chosen because block sizes of 8 * 1024 * 1024=8388608 bytes are used by AzCopy
+        var blockSize = 8 * 1024 * 1024;
+        var blobRanges = buildRanges(sourceSize, blockSize);
+        blobRanges.parallelStream()
+                .forEach(range -> {
+                    // BlockId of the destination block
+                    var blockId = Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8));
+                    destBlobClient.stageBlockFromUrl(blockId, sourceBlobUrl, range);
+                });
+
+        destBlobClient.commitBlockList(blockIds);
+        System.out.println("End " + java.time.LocalDateTime.now());
+    }
+
+    /*
+    * Given the size of a blob, return the list of blob ranges to cut the blob into equal parts.
+    * The last block can have a different size.
+    * */
+    private List<BlobRange> buildRanges(long blobSize, long blockSize) {
+        var offset = 0;
+        List<BlobRange> ranges = new ArrayList<>();
+        while (offset < blobSize) {
+            // Block size will be of size `blobSize - offset` if it is the last block of the blob.
+            var currentBlockSize = Math.min(blockSize, blobSize - offset);
+            ranges.add(new BlobRange(offset, currentBlockSize));
+            offset += blockSize;
+        }
+        return ranges;
+    }
 }
 
