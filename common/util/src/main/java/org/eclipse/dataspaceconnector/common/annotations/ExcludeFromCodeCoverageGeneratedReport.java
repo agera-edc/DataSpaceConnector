ed29c772db9527905e9b88044a7e82ff6b7bdb36
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-03-02 17:10:24 +0100

    Update test

---
+++ common/util/src/main/java/org/eclipse/dataspaceconnector/common/annotations/ExcludeFromCodeCoverageGeneratedReport.java
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2021 Copyright Holder (Catena-X Consortium)
+//
+// See the AUTHORS file(s) distributed with this work for additional
+// information regarding authorship.
+//
+// See the LICENSE file(s) distributed with this work for
+// additional information regarding license terms.
+//
+package org.eclipse.dataspaceconnector.common.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Exclude a block of code from code coverage.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface ExcludeFromCodeCoverageGeneratedReport {
+}
