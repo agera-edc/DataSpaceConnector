6d25aea3c081b1909c3db41fb86e84b45e49f504
Alexandre Gattiker <algattik@microsoft.com>
2022-03-03 10:19:19 +0100

    .

--- extensions/data-plane/data-plane-spi/src/main/java/org/eclipse/dataspaceconnector/dataplane/spi/pipeline/PipelineService.java
+++ extensions/data-plane/data-plane-spi/src/main/java/org/eclipse/dataspaceconnector/dataplane/spi/pipeline/PipelineService.java
@@ -14,7 +14,6 @@
 package org.eclipse.dataspaceconnector.dataplane.spi.pipeline;
 
 import org.eclipse.dataspaceconnector.dataplane.spi.result.TransferResult;
-import org.eclipse.dataspaceconnector.spi.result.Result;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataFlowRequest;
 
 import java.util.concurrent.CompletableFuture;
@@ -23,16 +22,6 @@ import java.util.concurrent.CompletableFuture;
  * Transfers data from a source to a sink.
  */
 public interface PipelineService {
-
-    /**
-     * Determines if the request is valid.
-     */
-    Result<Boolean> validate(DataFlowRequest request);
-
-    /**
-     * Transfers data associated with the request.
-     */
-    CompletableFuture<TransferResult> transfer(DataFlowRequest request);
 
     /**
      * Transfers data using the supplied data source.
