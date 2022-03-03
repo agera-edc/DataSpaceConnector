304128b16901338b3844d7da9c6ef0c67a312745
Alexandre Gattiker <algattik@microsoft.com>
2022-03-03 08:17:49 +0100

    Refactored into smaller methods

--- system-tests/tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/utils/FileTransferSimulationUtils.java
+++ system-tests/tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/utils/FileTransferSimulationUtils.java
@@ -14,7 +14,6 @@
 
 package org.eclipse.dataspaceconnector.system.tests.utils;
 
-import com.github.javafaker.Faker;
 import io.gatling.javaapi.core.ChainBuilder;
 import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.types.domain.contract.negotiation.ContractNegotiationStates;
@@ -23,7 +22,6 @@ import org.eclipse.dataspaceconnector.spi.types.domain.transfer.TransferProcessS
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.Duration;
-import java.util.Map;
 import java.util.Objects;
 
 import static io.gatling.javaapi.core.CoreDsl.StringBody;
@@ -36,15 +34,15 @@ import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 import static java.lang.String.format;
-import static org.eclipse.dataspaceconnector.system.tests.utils.GatlingUtils.endlesslyWith;
 
 /**
  * Utility methods for building a Gatling simulation for performing contract negotiation and file transfer.
  */
 public abstract class FileTransferSimulationUtils {
 
-    private FileTransferSimulationUtils() {
-    }
+    public static final String CONTRACT_AGREEMENT_ID = "contractAgreementId";
+    public static final String CONTRACT_NEGOTIATION_REQUEST_ID = "contractNegotiationRequestId";
+    public static final String TRANSFER_PROCESS_ID = "transferProcessId";
 
     public static final String DESCRIPTION = "[Contract negotiation and file transfer]";
     public static final String OFFER_FILE = "contractoffer.json";
@@ -56,7 +54,8 @@ public abstract class FileTransferSimulationUtils {
     private static final String CONTRACT_ID_PARAM = "contractId";
     private static final String API_KEY_HEADER = "X-Api-Key";
 
-    private static final Faker FAKER = new Faker();
+    private FileTransferSimulationUtils() {
+    }
 
     /**
      * Gatling chain for performing contract negotiation and file transfer.
@@ -66,39 +65,58 @@ public abstract class FileTransferSimulationUtils {
      * @param apiKey          Consumer runtime API Key.
      */
     public static ChainBuilder contractNegotiationAndFileTransfer(String providerUrl, String destinationPath, String apiKey) {
+        return startContractAgreement(providerUrl, loadContractAgreement())
+                .exec(waitForContractAgreement(apiKey))
+                .exec(startFileTransfer(providerUrl, destinationPath))
+                .exec(waitForTransferCompletion());
+    }
+
+    /**
+     * Gatling chain for initiating a contract negotiation.
+     * <p>
+     * Saves the Contract Negotiation Request ID into the {@see CONTRACT_NEGOTIATION_REQUEST_ID} session key.
+     *
+     * @param providerUrl       URL for the Provider API, as accessed from the Consumer runtime.
+     * @param contractAgreement Contract agreement definition in JSON format.
+     */
+    private static ChainBuilder startContractAgreement(String providerUrl, String contractAgreement) {
         String connectorAddress = format("%s/api/ids/multipart", providerUrl);
-        String body;
-        try {
-            InputStream offerFileData = Thread.currentThread().getContextClassLoader().getResourceAsStream(OFFER_FILE);
-            body = new String(Objects.requireNonNull(offerFileData, OFFER_FILE + " not found").readAllBytes());
-        } catch (IOException e) {
-            throw new EdcException(e);
-        }
         return group("Contract negotiation")
                 .on(exec(
                         // Initiate a contract negotiation
                         http("Contract negotiation")
                                 .post("/api/negotiation")
-                                .body(StringBody(body))
+                                .body(StringBody(contractAgreement))
                                 .header(CONTENT_TYPE, "application/json")
                                 .queryParam(CONNECTOR_ADDRESS_PARAM, connectorAddress)
                                 .check(status().is(200))
                                 .check(bodyString()
                                         .notNull()
-                                        // UUID is returned to get the contract agreement negotiated between provider and consumer.
-                                        .saveAs("contractNegotiationRequestId"))
-                ))
-                // Call ContractNegotiation status endpoint every second, till it gives a CONFIRMED state.
-                .exec(session -> session.set("status", -1))
+                                        .saveAs(CONTRACT_NEGOTIATION_REQUEST_ID))
+                ));
+    }
+
+    /**
+     * Gatling chain for calling ContractNegotiation status endpoint repeatedly until a CONFIRMED state is
+     * attained, or a timeout is reached.
+     * <p>
+     * Expects the Contract Negotiation Request ID to be provided in the {@see CONTRACT_NEGOTIATION_REQUEST_ID} session key.
+     * <p>
+     * Saves the Contract Agreement ID into the {@see CONTRACT_AGREEMENT_ID} session key.
+     *
+     * @param apiKey Consumer runtime API Key.
+     */
+    private static ChainBuilder waitForContractAgreement(String apiKey) {
+        return exec(session -> session.set("status", -1))
                 .group("Wait for agreement").on(
-                        doWhileDuring(session -> session.getString("contractAgreementId") == null,
+                        doWhileDuring(session -> session.getString(CONTRACT_AGREEMENT_ID) == null,
                                 Duration.ofSeconds(30))
                                 .on(exec(http("Get status")
-                                                .get(session -> format("/api/control/negotiation/%s", session.getString("contractNegotiationRequestId")))
+                                                .get(session -> format("/api/control/negotiation/%s", session.getString(CONTRACT_NEGOTIATION_REQUEST_ID)))
                                                 .header(API_KEY_HEADER, apiKey)
                                                 .check(status().is(200))
                                                 .check(
-                                                        jmesPath("id").is(session -> session.getString("contractNegotiationRequestId")),
+                                                        jmesPath("id").is(session -> session.getString(CONTRACT_NEGOTIATION_REQUEST_ID)),
                                                         jmesPath("state").saveAs("status")
                                                 )
                                                 .checkIf(
@@ -106,40 +124,70 @@ public abstract class FileTransferSimulationUtils {
                                                 )
                                                 .then(
                                                         // Obtain contract agreement ID
-                                                        jmesPath("contractAgreement.id").notNull().saveAs("contractAgreementId")
+                                                        jmesPath("contractAgreement.id").notNull().saveAs(CONTRACT_AGREEMENT_ID)
                                                 )
                                         ).pace(Duration.ofSeconds(1))
                                 )
 
-                )
-                .feed(endlesslyWith(() -> Map.of("fileName", FAKER.lorem().characters(20, 40))))
-                .group("Initiate transfer")
+                );
+    }
+
+    /**
+     * Gatling chain for initiating a file transfer request.
+     * <p>
+     * Expects the Contract Agreement ID to be provided in the {@see CONTRACT_AGREEMENT_ID} session key.
+     * <p>
+     * Saves the Transfer Process ID into the {@see TRANSFER_PROCESS_ID} session key.
+     *
+     * @param providerUrl     URL for the Provider API, as accessed from the Consumer runtime.
+     * @param destinationPath File copy destination path. If it includes the character sequence {@code %s}, that sequence is replaced with a random string in each iteration.
+     */
+    private static ChainBuilder startFileTransfer(String providerUrl, String destinationPath) {
+        String connectorAddress = format("%s/api/ids/multipart", providerUrl);
+        return group("Initiate transfer")
                 .on(exec(
                         // Initiate a file transfer
                         http("Initiate transfer")
                                 .post(format("/api/file/%s", PROVIDER_ASSET_NAME))
                                 .queryParam(CONNECTOR_ADDRESS_PARAM, connectorAddress)
-                                .queryParam(DESTINATION_PARAM, s -> format(destinationPath, s.getString("fileName")))
-                                .queryParam(CONTRACT_ID_PARAM, s -> s.getString("contractAgreementId"))
+                                .queryParam(DESTINATION_PARAM, destinationPath)
+                                .queryParam(CONTRACT_ID_PARAM, s -> s.getString(CONTRACT_AGREEMENT_ID))
                                 .check(status().is(200))
                                 .check(bodyString()
                                         .notNull()
-                                        .saveAs("transferProcessId"))
-                ))
-                .exec(session -> session.set("status", -1))
-                // Call transfer status endpoint every second, till it gives a COMPLETED state.
-                .group("Wait for transfer").on(
-                        doWhileDuring(session -> session.getInt("status") != TransferProcessStates.COMPLETED.code(),
+                                        .saveAs(TRANSFER_PROCESS_ID))
+                ));
+    }
+
+    /**
+     * Gatling chain for calling the transfer status endpoint repeatedly until a COMPLETED state is
+     * attained, or a timeout is reached.
+     * <p>
+     * Expects the Transfer Process ID to be provided in the {@see TRANSFER_PROCESS_ID} session key.
+     */
+    private static ChainBuilder waitForTransferCompletion() {
+        return group("Wait for transfer").on(
+                exec(session -> session.set("status", -1))
+                        .doWhileDuring(session -> session.getInt("status") != TransferProcessStates.COMPLETED.code(),
                                 Duration.ofSeconds(30))
-                                .on(exec(http("Get transfer status")
-                                                .get(session -> format("/api/transfer/%s", session.getString("transferProcessId")))
-                                                .check(status().is(200))
-                                                .check(
-                                                        jmesPath("id").is(session -> session.getString("transferProcessId")),
-                                                        jmesPath("state").saveAs("status")
-                                                )
-                                        ).pace(Duration.ofSeconds(1))
-                                )
-                );
+                        .on(exec(http("Get transfer status")
+                                        .get(session -> format("/api/transfer/%s", session.getString(TRANSFER_PROCESS_ID)))
+                                        .check(status().is(200))
+                                        .check(
+                                                jmesPath("id").is(session -> session.getString(TRANSFER_PROCESS_ID)),
+                                                jmesPath("state").saveAs("status")
+                                        )
+                                ).pace(Duration.ofSeconds(1))
+                        )
+        );
+    }
+
+    private static String loadContractAgreement() {
+        try {
+            InputStream offerFileData = Thread.currentThread().getContextClassLoader().getResourceAsStream(OFFER_FILE);
+            return new String(Objects.requireNonNull(offerFileData, OFFER_FILE + " not found").readAllBytes());
+        } catch (IOException e) {
+            throw new EdcException(e);
+        }
     }
 }
