ed701e24067beb9f1fdbdcb4d8b1c47164cdc567
Alexandre Gattiker <algattik@microsoft.com>
2022-03-04 14:14:22 +0100

    Revert "Update DataPlaneFrameworkExtensionTest.java"
    This reverts commit 507c4a783d368df5a58447b56ca6e023cf594db6.

--- extensions/data-plane/data-plane-framework/src/test/java/org/eclipse/dataspaceconnector/dataplane/framework/DataPlaneFrameworkExtensionTest.java
+++ extensions/data-plane/data-plane-framework/src/test/java/org/eclipse/dataspaceconnector/dataplane/framework/DataPlaneFrameworkExtensionTest.java
@@ -2,24 +2,15 @@ package org.eclipse.dataspaceconnector.dataplane.framework;
 
 import org.eclipse.dataspaceconnector.boot.system.injection.ReflectiveObjectFactory;
 import org.eclipse.dataspaceconnector.dataplane.framework.manager.DataPlaneManagerImpl;
-import org.eclipse.dataspaceconnector.dataplane.framework.manager.TransferServiceSelectionStrategy;
 import org.eclipse.dataspaceconnector.dataplane.framework.pipeline.PipelineServiceImpl;
 import org.eclipse.dataspaceconnector.dataplane.spi.manager.DataPlaneManager;
 import org.eclipse.dataspaceconnector.dataplane.spi.pipeline.PipelineService;
 import org.eclipse.dataspaceconnector.junit.launcher.DependencyInjectionExtension;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
-import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataFlowRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.stream.Stream;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.dataspaceconnector.dataplane.framework.e2e.EndToEndTest.createRequest;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(DependencyInjectionExtension.class)
 class DataPlaneFrameworkExtensionTest {
@@ -34,25 +25,9 @@ class DataPlaneFrameworkExtensionTest {
     }
 
     @Test
-    void initialize_registers_DataPlaneManager_withDefaultStrategy(ServiceExtensionContext context, ReflectiveObjectFactory factory) {
+    void initialize_registers_DataPlaneManager(ServiceExtensionContext context, ReflectiveObjectFactory factory) {
         var extension = factory.constructInstance(DataPlaneFrameworkExtension.class);
         extension.initialize(context);
         assertThat(context.getService(DataPlaneManager.class)).isInstanceOf(DataPlaneManagerImpl.class);
-    }
-
-    @Test
-    void initialize_registers_DataPlaneManager_withProvidedStrategy(ServiceExtensionContext context, ReflectiveObjectFactory factory) {
-        // Arrange
-        TransferServiceSelectionStrategy strategy = mock(TransferServiceSelectionStrategy.class);
-        context.registerService(TransferServiceSelectionStrategy.class, strategy);
-
-        // Act
-        var extension = factory.constructInstance(DataPlaneFrameworkExtension.class);
-        extension.initialize(context);
-        DataFlowRequest request = createRequest("1").build();
-        context.getService(DataPlaneManager.class).validate(request);
-
-        // Assert
-        verify(strategy).chooseTransferService(eq(request), any(Stream.class));
     }
 }
