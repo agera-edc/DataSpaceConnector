a4c16ceb1d418943d30c8f8c755d6009d3600860
Alexandre Gattiker <algattik@microsoft.com>
2022-03-04 13:00:38 +0100

    Use DI extension

--- extensions/data-plane/data-plane-framework/src/test/java/org/eclipse/dataspaceconnector/dataplane/framework/DataPlaneFrameworkExtensionTest.java
+++ extensions/data-plane/data-plane-framework/src/test/java/org/eclipse/dataspaceconnector/dataplane/framework/DataPlaneFrameworkExtensionTest.java
@@ -1,31 +1,33 @@
 package org.eclipse.dataspaceconnector.dataplane.framework;
 
+import org.eclipse.dataspaceconnector.boot.system.injection.ReflectiveObjectFactory;
 import org.eclipse.dataspaceconnector.dataplane.framework.manager.DataPlaneManagerImpl;
 import org.eclipse.dataspaceconnector.dataplane.framework.pipeline.PipelineServiceImpl;
 import org.eclipse.dataspaceconnector.dataplane.spi.manager.DataPlaneManager;
 import org.eclipse.dataspaceconnector.dataplane.spi.pipeline.PipelineService;
+import org.eclipse.dataspaceconnector.junit.launcher.DependencyInjectionExtension;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(DependencyInjectionExtension.class)
 class DataPlaneFrameworkExtensionTest {
 
-    DataPlaneFrameworkExtension extension = new DataPlaneFrameworkExtension(null);
-    ServiceExtensionContext context = mock(ServiceExtensionContext.class);
 
     @Test
-    void initialize_registers_PipelineService() {
+    void initialize_registers_PipelineService(ServiceExtensionContext context, ReflectiveObjectFactory factory) {
+        var extension = factory.constructInstance(DataPlaneFrameworkExtension.class);
         extension.initialize(context);
-        verify(context).registerService(eq(PipelineService.class), any(PipelineServiceImpl.class));
+
+        assertThat(context.getService(PipelineService.class)).isInstanceOf(PipelineServiceImpl.class);
     }
 
     @Test
-    void initialize_registers_DataPlaneManager() {
+    void initialize_registers_DataPlaneManager(ServiceExtensionContext context, ReflectiveObjectFactory factory) {
+        var extension = factory.constructInstance(DataPlaneFrameworkExtension.class);
         extension.initialize(context);
-        verify(context).registerService(eq(DataPlaneManager.class), any(DataPlaneManagerImpl.class));
+        assertThat(context.getService(DataPlaneManager.class)).isInstanceOf(DataPlaneManagerImpl.class);
     }
 }
