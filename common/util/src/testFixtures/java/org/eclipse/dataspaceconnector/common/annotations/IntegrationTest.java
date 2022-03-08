c453283c559b30d02fb48760c197f906f5806d87
chrislomonico <40393739+chrislomonico@users.noreply.github.com>
2022-03-08 08:58:28 +0100

    Update common/util/src/testFixtures/java/org/eclipse/dataspaceconnector/common/annotations/IntegrationTest.java

--- common/util/src/testFixtures/java/org/eclipse/dataspaceconnector/common/annotations/IntegrationTest.java
+++ common/util/src/testFixtures/java/org/eclipse/dataspaceconnector/common/annotations/IntegrationTest.java
@@ -23,7 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Common annotation for integration testing.It applies integration-test Junit Tag.
+ * Common annotation for integration testing.  It applies integration-test Junit Tag.
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
