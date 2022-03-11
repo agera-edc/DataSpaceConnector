94e72498375cece2c8d86049fa454f4a3c56833a
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-03-11 18:28:14 +0100

    Update javadoc

--- spi/contract-spi/src/main/java/org/eclipse/dataspaceconnector/spi/contract/policy/store/PolicyStore.java
+++ spi/contract-spi/src/main/java/org/eclipse/dataspaceconnector/spi/contract/policy/store/PolicyStore.java
@@ -13,23 +13,31 @@ public interface PolicyStore {
 
     /**
      * Returns the policy by id or returns null if not found.
+     *
+     * @param policyId id of the policy.
      */
     @Nullable
     Policy findById(String policyId);
 
     /**
      * Returns stream of policies in the store based on query spec.
+     *
+     * @param spec query specification.
      */
     Stream<Policy> findAll(QuerySpec spec);
 
     /**
      * Persists the policy.
+     *
+     * @param policy to be saved.
      */
     void save(Policy policy);
 
     /**
      * Removes a policy for the given id.
      * Returns deleted policy or null if policy not found.
+     *
+     * @param policyId id of the policy to be removed.
      */
     Policy delete(String policyId);
 }
