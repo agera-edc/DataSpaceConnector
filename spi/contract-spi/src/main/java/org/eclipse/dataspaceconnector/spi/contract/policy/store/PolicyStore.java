f227a289015e60dfdf2f8fbab0cdb7ddd1e8b41a
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-03-14 15:46:06 +0100

    Add javadocs

--- spi/contract-spi/src/main/java/org/eclipse/dataspaceconnector/spi/contract/policy/store/PolicyStore.java
+++ spi/contract-spi/src/main/java/org/eclipse/dataspaceconnector/spi/contract/policy/store/PolicyStore.java
@@ -1,6 +1,7 @@
 package org.eclipse.dataspaceconnector.spi.contract.policy.store;
 
 import org.eclipse.dataspaceconnector.policy.model.Policy;
+import org.eclipse.dataspaceconnector.spi.persistence.EdcPersistenceException;
 import org.eclipse.dataspaceconnector.spi.query.QuerySpec;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,17 +13,21 @@ import java.util.stream.Stream;
 public interface PolicyStore {
 
     /**
-     * Returns the policy by id or returns null if not found.
+     * Findss the policy by id.
      *
      * @param policyId id of the policy.
+     * @return found Policy or null if not found.
+     * @throws EdcPersistenceException if something goes wrong.
      */
     @Nullable
     Policy findById(String policyId);
 
     /**
-     * Returns stream of policies in the store based on query spec.
+     * Find stream of policies in the store based on query spec.
      *
      * @param spec query specification.
+     * @return stream of found policies.
+     * @throws EdcPersistenceException if something goes wrong.
      */
     Stream<Policy> findAll(QuerySpec spec);
 
@@ -30,14 +35,16 @@ public interface PolicyStore {
      * Persists the policy.
      *
      * @param policy to be saved.
+     * @throws EdcPersistenceException if something goes wrong.
      */
     void save(Policy policy);
 
     /**
-     * Removes a policy for the given id.
-     * Returns deleted policy or null if policy not found.
+     * Deletes a policy for the given id.
      *
      * @param policyId id of the policy to be removed.
+     * @return deleted policy or null if policy not found.
+     * @throws EdcPersistenceException if something goes wrong.
      */
     Policy delete(String policyId);
 }
