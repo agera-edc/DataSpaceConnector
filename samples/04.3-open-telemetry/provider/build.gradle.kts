c6cfbf0fbcb27ab9d7d00e7fad8f1f74f261638b
Alexandre Gattiker <algattik@microsoft.com>
2022-03-17 15:50:33 +0100

    .

--- samples/04.3-open-telemetry/provider/build.gradle.kts
+++ samples/04.3-open-telemetry/provider/build.gradle.kts
@@ -9,6 +9,7 @@
  *
  *  Contributors:
  *       Microsoft Corporation - initial API and implementation
+ *       Fraunhofer Institute for Software and Systems Engineering - added dependencies
  *       ZF Friedrichshafen AG - add dependency
  *
  */
@@ -24,25 +25,23 @@ val rsApi: String by project
 
 dependencies {
     implementation(project(":core"))
-    implementation(project(":core:micrometer"))
 
     implementation(project(":extensions:in-memory:assetindex-memory"))
     implementation(project(":extensions:in-memory:transfer-store-memory"))
-    implementation(project(":extensions:in-memory:assetindex-memory"))
     implementation(project(":extensions:in-memory:negotiation-store-memory"))
     implementation(project(":extensions:in-memory:contractdefinition-store-memory"))
+
+    implementation(project(":extensions:api:observability"))
 
     implementation(project(":extensions:filesystem:configuration-fs"))
     implementation(project(":extensions:iam:iam-mock"))
 
-    implementation(project(":extensions:api:control"))
     implementation(project(":extensions:api:data-management"))
 
     implementation(project(":data-protocols:ids"))
-    runtimeOnly(project(":extensions:http:jersey-micrometer"))
-    runtimeOnly(project(":extensions:http:jetty-micrometer"))
+
+    implementation(project(":samples:04.0-file-transfer:transfer-file"))
     runtimeOnly(project(":extensions:jdk-logger-monitor"))
-    implementation(project(":samples:04.0-file-transfer:api"))
 }
 
 application {
@@ -52,5 +51,5 @@ application {
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     exclude("**/pom.properties", "**/pom.xm")
     mergeServiceFiles()
-    archiveFileName.set("consumer.jar")
+    archiveFileName.set("provider.jar")
 }
