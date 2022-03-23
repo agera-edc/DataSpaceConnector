68baf822df915faf610771bdcdcdde0d3eb72de2
Chris Lomonico <chris.lomonico@microsoft.com>
2022-03-23 06:47:06 +0100

    initial spike commit for 121

---
+++ docs/developer/decision-records/2022-03-21-maven-central-deployment/README.md
@@ -0,0 +1,178 @@
+# Publishing Artifacts To Maven Central
+
+## Decision
+
+Enable the publishing of artifacts to Maven Central from the [Eclipse Data Space GitHub Repository](https://github.com/eclipse-dataspaceconnector/DataSpaceConnector)
+
+## Rationale
+
+A workflow in the EDC repository should exist to build and publish release artifacts to Maven Central to enable
+developers to utilize the tested and verified libraries. TODO
+
+## Steps
+In order to be able to deploy to Maven Central, the following steps must be carried out
+
+### Jira Account
+- Create a SonarType Jira Account [here](https://issues.sonatype.org/secure/Signup!default.jspa)
+
+### Create the Request for OSSRH
+- Using the [pre-defined template](https://issues.sonatype.org/secure/CreateIssue.jspa?issuetype=21&pid=10134) create 
+a new Jira Issue
+  - Provide all required fields including a _unique group id_ that will be used to identify the main package provider
+  - Additional guidance on the suggested naming approach can be found [here](https://central.sonatype.org/publish/requirements/coordinates/)
+- Submit the issue and await the instructions
+  - Wait time seems to be minimal (less than 5 minutes)
+  - If there are any issues or errors or changes required:
+    - Fix issues
+    - Click `Respond` on Jira issue and await further instructions
+- Upon successful completion the related Jira issue will be updated to a state of `Resolved` 
+  - In activity an entry will be present indicating that the `users` specified during ticket creation in the `Usernames` 
+field will have the ability to publish snapshot/release artifacts to the `OSSRH Respoistory`
+  - Full instructions on deployment procedures with details can be found [here](https://central.sonatype.org/publish/publish-guide/#deployment)
+- Note that it can take up to 2 days for the OSSRH
+![Example: Issue Creation](001-create-issue.png)
+
+- A generated update to the Jira ticket will be issued indicating that you will need to create a temporary repository
+with the specified name in the ticket to ensure verification of ownership
+
+### Signing Key
+
+If no key exists, one can use `gpg` to generate the key with the following command:
+
+```script
+gpg --gen-key
+```
+
+Follow the instructions and provide the same information used for registration
+
+- Real Name
+- Email
+
+Export the public part of the key
+
+```ecma script level 4
+gpg --armor --output public-key.gpg --export [email addres used to generate key]
+```
+View the exported key file `public-key.gpg` and ensure that it starts with `-----BEGIN PGP PUBLIC KEY BLOCK-----`
+
+Publish the key to _at least 1_ well-known key server.
+
+> For Example:
+> 
+>https://keyserver.ubuntu.com
+>
+> or
+> 
+> https://keys.openpgp.org
+
+Follow the instructions to upload your public key.
+- First provide key file or payload of key file pending on public key server you are using
+- Execute any required verification steps (such as email confirmation)
+- Verify that your key is found in the related key server  
+
+### Gradle
+
+Changes must be made to `build.gradle.kts` in the `root` of the project
+
+> Note: assumption is that Kotlin DSL is being used
+
+### Plugin Configuration
+
+If not present add 
+- `maven-publish`
+- `id("io.github.gradle-nexus.publish-plugin") version "1.1.0"`
+to the `plugins`
+
+Example: 
+```java
+plugins {
+        `java-library`
+        `maven-publish`
+        checkstyle
+        jacoco
+        id("com.rameshkp.openapi-merger-gradle-plugin") version "1.0.4"
+        id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+        }
+```
+### Repository Configuration
+
+Ensure that `repositories`  includes `mavenCentral()`
+
+For Example:
+```java
+repositories {
+    mavenCentral()
+}
+```
+
+### Java Plugin
+
+Maven Central requires `Javadocs` and `sources` to be published with the `JAR` files.  If this both are not present
+the validation process with fail.
+
+Configure the `Java` extension to include tasks for `sources` and `javadoc`
+
+For Example:
+```java
+java{
+    withJavadocJar()
+    withSourcesJar()
+}
+```
+
+### OSSHR Publishing
+
+Include the configuration for `nexusPublishing` and for the `sonatype` repository, set the `username` and the `password`
+for the automation of creating/closing and releasing staging repositories.  
+
+For Example:
+```java
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("OSSRH_USER") ?: return@sonatype)
+            password.set(System.getenv("OSSRH_PASSWORD") ?: return@sonatype)
+        }
+    }
+}
+```
+
+Update the `repositories` under the `java-library`, `publishing` extension to include the `OSSRH` staging repo
+
+For Example:
+```java
+pluginManager.withPlugin("java-library"){
+    //....
+        publishing {
+            repositories {        
+                mavenCentral{
+                    name="OSSRH"
+                    url=uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    credentials{
+                        username=System.getenv("OSSRH_USER")
+                        password=System.getenv("OSSRH_PASSWORD")
+                    }
+                }
+            }
+        }
+    //....        
+}
+```
+
+- The username and password are the same for what was used to create the OSSRH request (same as `Jira` credentials)
+- Values for `OSSRH_USER` and `OSSRH_PASSWORD` must be defined and should be protected in the build environment
+- The url: https://s01.oss.sonatype.org/content/repositories/snapshots/ is used for staging snapshots
+- The url: https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ is used for release deployements
+- For detailed information please see [OSSRH Guide - Getting Started](https://central.sonatype.org/publish/publish-guide/#accessing-repositories)
+  
+## Remaining Tasks
+
+- Once OSSRH testing repo is complete, test and implement the approach documented
+- Review `build.gradle.kts`  
+  - R
+
+
+
+
+
+
---
+++ docs/developer/decision-records/2022-03-21-maven-central-deployment/example.build.gradle.kts
@@ -0,0 +1,283 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+plugins {
+    `java-library`
+    `maven-publish`
+    checkstyle
+    jacoco
+    id("com.rameshkp.openapi-merger-gradle-plugin") version "1.0.4"
+
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("OSSRH_USER") ?: return@sonatype)
+            password.set(System.getenv("OSSRH_PASSWORD") ?: return@sonatype)
+        }
+    }
+}
+
+val jetBrainsAnnotationsVersion: String by project
+val jacksonVersion: String by project
+val javaVersion: String by project
+val jupiterVersion: String by project
+val mockitoVersion: String by project
+val assertj: String by project
+val rsApi: String by project
+val swagger: String by project
+val faker: String by project
+
+val groupId: String = "org.eclipse.dataspaceconnector"
+var edcVersion: String = "0.0.1-SNAPSHOT"
+
+if (project.version == "unspecified") {
+    logger.warn("No version was specified, setting default 0.0.1-SNAPSHOT")
+    logger.warn("If you want to change this, supply the -Pversion=X.Y.Z parameter")
+    logger.warn("")
+} else {
+    edcVersion = project.version as String
+}
+java{
+    withJavadocJar()
+    withSourcesJar()
+}
+
+subprojects {
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.iais.fraunhofer.de/artifactory/eis-ids-public/")
+        }
+    }
+
+
+
+    tasks.register<DependencyReportTask>("allDependencies") {}
+}
+
+buildscript {
+    dependencies {
+        classpath("io.swagger.core.v3:swagger-gradle-plugin:2.1.13")
+    }
+}
+
+allprojects {
+    apply(plugin = "maven-publish")
+    apply(plugin = "checkstyle")
+    apply(plugin = "java")
+
+    if (System.getenv("JACOCO") == "true") {
+        apply(plugin = "jacoco")
+    }
+
+    checkstyle {
+        toolVersion = "9.0"
+        configFile = rootProject.file("resources/edc-checkstyle-config.xml")
+        maxErrors = 0 // does not tolerate errors ...
+        maxWarnings = 0 // ... or warnings
+    }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(javaVersion))
+        }
+    }
+
+    // EdcRuntimeExtension uses this to determine the runtime classpath of the module to run.
+    tasks.register("printClasspath") {
+        doLast {
+            println(sourceSets["main"].runtimeClasspath.asPath);
+        }
+    }
+
+    pluginManager.withPlugin("java-library") {
+        group = groupId
+        version = edcVersion
+
+        dependencies {
+            api("org.jetbrains:annotations:${jetBrainsAnnotationsVersion}")
+            api("com.fasterxml.jackson.core:jackson-core:${jacksonVersion}")
+            api("com.fasterxml.jackson.core:jackson-annotations:${jacksonVersion}")
+            api("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
+            api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${jacksonVersion}")
+
+            testImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
+            testImplementation("org.junit.jupiter:junit-jupiter-params:${jupiterVersion}")
+            testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${jupiterVersion}")
+            testImplementation("org.mockito:mockito-core:${mockitoVersion}")
+            testImplementation("org.assertj:assertj-core:${assertj}")
+            testImplementation("com.github.javafaker:javafaker:${faker}")
+        }
+
+        publishing {
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/eclipse-dataspaceconnector/DataSpaceConnector")
+                    credentials {
+                        username = System.getenv("GITHUB_ACTOR")
+                        password = System.getenv("GITHUB_TOKEN")
+                    }
+                }
+                mavenCentral{
+                    name = "OSSRH"
+                    url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    credentials {
+                        username = System.getenv("OSSRH_USER")
+                        password = System.getenv("OSSRH_PASSWORD")
+                    }
+                }
+            }
+
+            publications{
+
+
+                name.set(" :: ${project.name}")
+                description.set("aws-junit5 :: ${project.name}")
+                url.set("https://github.com/madhead/aws-junit5")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id = "[dev id]"
+                        name = "[dev name]"
+                        email = "[dev email]"
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:madhead/aws-junit5.git")
+                    url.set("https://github.com/madhead/aws-ju
+                }
+            }
+
+        }
+
+    }
+
+    pluginManager.withPlugin("io.swagger.core.v3.swagger-gradle-plugin") {
+
+        dependencies {
+            // this is used to scan the classpath and generate an openapi yaml file
+            implementation("io.swagger.core.v3:swagger-jaxrs2-jakarta:${swagger}")
+            implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
+        }
+// this is used to scan the classpath and generate an openapi yaml file
+        tasks.withType<io.swagger.v3.plugins.gradle.tasks.ResolveTask> {
+            outputFileName = project.name
+            outputFormat = io.swagger.v3.plugins.gradle.tasks.ResolveTask.Format.YAML
+            prettyPrint = true
+            classpath = java.sourceSets["main"].runtimeClasspath
+            buildClasspath = classpath
+            resourcePackages = setOf("org.eclipse.dataspaceconnector")
+            outputDir = file("${rootProject.projectDir.path}/resources/openapi/yaml")
+        }
+        configurations {
+            all {
+                exclude(group = "com.fasterxml.jackson.jaxrs", module = "jackson-jaxrs-json-provider")
+            }
+        }
+    }
+
+    tasks.withType<Test> {
+        // Target all type of test e.g. -DrunAllTests="true"
+        val runAllTests: String = System.getProperty("runAllTests", "false");
+        if (runAllTests == "true") {
+            useJUnitPlatform()
+        } else {
+            // Target specific set of tests by specifying junit tags on command-line e.g. -DincludeTags="tag-name1,tag-name2"
+            val includeTagProperty = System.getProperty("includeTags");
+            val includeTags: Array<String> = includeTagProperty?.split(",")?.toTypedArray() ?: emptyArray();
+
+            if (includeTags.isNotEmpty()) {
+                useJUnitPlatform {
+                    includeTags(*includeTags)
+                }
+            } else {
+                useJUnitPlatform {
+                    excludeTags("IntegrationTest")
+                }
+            }
+        }
+    }
+
+    tasks.withType<Test> {
+        testLogging {
+            events("failed")
+            showStackTraces = true
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
+    }
+    tasks.withType<Checkstyle> {
+        reports {
+            // lets not generate any reports because that is done from within the Github Actions workflow
+            html.required.set(false)
+            xml.required.set(false)
+        }
+    }
+
+    tasks.jar {
+        metaInf {
+            from("${rootProject.projectDir.path}/LICENSE")
+            from("${rootProject.projectDir.path}/NOTICE.md")
+        }
+    }
+
+    // Generate XML reports for Codecov
+    if (System.getenv("JACOCO") == "true") {
+        tasks.jacocoTestReport {
+            reports {
+                xml.required.set(true)
+            }
+        }
+    }
+}
+
+openApiMerger {
+    val yamlDirectory = file("${rootProject.projectDir.path}/resources/openapi/yaml")
+
+    inputDirectory.set(yamlDirectory)
+    output {
+        directory.set(file("${rootProject.projectDir.path}/resources/openapi/"))
+        fileName.set("openapi")
+        fileExtension.set("yaml")
+    }
+    openApi {
+        openApiVersion.set("3.0.1")
+        info {
+            title.set("EDC REST API")
+            description.set("All files merged by open api merger")
+            version.set("1.0.0-SNAPSHOT")
+            license {
+                name.set("Apache License v2.0")
+                url.set("http://apache.org/v2")
+            }
+        }
+    }
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform()
+}
