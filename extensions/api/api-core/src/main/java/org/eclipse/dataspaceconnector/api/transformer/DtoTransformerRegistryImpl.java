a47521e6cbf30943b40cf57c301ac49d701cdcb6
Alexandre Gattiker <algattik@microsoft.com>
2022-03-25 05:34:52 +0100

    compat

--- extensions/api/api-core/src/main/java/org/eclipse/dataspaceconnector/api/transformer/DtoTransformerRegistryImpl.java
+++ extensions/api/api-core/src/main/java/org/eclipse/dataspaceconnector/api/transformer/DtoTransformerRegistryImpl.java
@@ -77,7 +77,8 @@ public class DtoTransformerRegistryImpl implements DtoTransformerRegistry {
     private <INPUT, OUTPUT> OUTPUT transform(INPUT object, Class<OUTPUT> outputType, TransformerContext context) {
         Objects.requireNonNull(object);
 
-        var t = transformers.stream().filter(tr -> tr.canHandle(object, outputType))
+        var t = transformers.stream()
+                .filter(tr -> tr.getInputType().isInstance(object) && tr.getOutputType().equals(outputType))
                 .findFirst().orElse(null);
 
         if (t == null) {
@@ -86,11 +87,11 @@ public class DtoTransformerRegistryImpl implements DtoTransformerRegistry {
         return outputType.cast(((DtoTransformer<INPUT, OUTPUT>) t).transform(object, context));
     }
 
-    private static class DtoTransformerContext implements TransformerContext {
+    public static class DtoTransformerContext implements TransformerContext {
         private final DtoTransformerRegistry registry;
         private final List<String> problems;
 
-        private DtoTransformerContext(DtoTransformerRegistry registry) {
+        public DtoTransformerContext(DtoTransformerRegistry registry) {
             this.registry = registry;
             problems = new ArrayList<>();
         }
