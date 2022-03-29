566bdc8d73800fe596b827c7ad02d443ff238890
Alexandre Gattiker <algattik@microsoft.com>
2022-03-29 13:19:51 +0200

    Added IoC details in Javadoc

--- spi/core-spi/src/main/java/org/eclipse/dataspaceconnector/spi/system/ExecutorInstrumentationImplementation.java
+++ spi/core-spi/src/main/java/org/eclipse/dataspaceconnector/spi/system/ExecutorInstrumentationImplementation.java
@@ -19,6 +19,15 @@ package org.eclipse.dataspaceconnector.spi.system;
  * A distinct interface is needed for dependency injection to resolve the optional
  * {@link ExecutorInstrumentationImplementation} vs. the mandatory {@link ExecutorInstrumentation}
  * that is provided by core services, providing a default implementation if none is provided.
+ * <p>
+ * The Micrometer extension provides a {@link ExecutorInstrumentationImplementation}.
+ * The core extension consumes an (optional) {@link ExecutorInstrumentationImplementation}
+ * and registers a {@link ExecutorInstrumentation}
+ * (with either the {@link ExecutorInstrumentationImplementation} instance or a default implementation).
+ * Downstream modules consume a (mandatory) {@link ExecutorInstrumentation}.
+ * <p>
+ * Because of the way dependencies are resolved (with a graph) it's not possible for
+ * the core extension to consume and register the same service.
  */
 public interface ExecutorInstrumentationImplementation extends ExecutorInstrumentation {
 }
