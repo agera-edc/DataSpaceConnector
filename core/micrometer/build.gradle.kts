614a52dfdfdf5bfb1ad30e1445757630157e5d6c
Ophelie Le Mentec <olementec@microsoft.com>
2022-03-31 10:42:14 +0200

    Removed dependency.

--- core/micrometer/build.gradle.kts
+++ core/micrometer/build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
     api("io.micrometer:micrometer-core:${micrometerVersion}")
 
     testImplementation(project(":extensions:api:observability"))
-    testImplementation(testFixtures(project(":extensions:azure:azure-test")))
     testImplementation(testFixtures(project(":launchers:junit")))
     testImplementation(testFixtures(project(":common:util")))
 
