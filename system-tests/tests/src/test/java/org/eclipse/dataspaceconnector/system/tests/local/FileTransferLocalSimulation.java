f3be7ce787fcd772da9710f41b287d9fe3e77cb5
Alexandre Gattiker <algattik@microsoft.com>
2022-04-26 08:01:54 +0200

    Update FileTransferLocalSimulation.java

--- system-tests/tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/local/FileTransferLocalSimulation.java
+++ system-tests/tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/local/FileTransferLocalSimulation.java
@@ -20,7 +20,7 @@ import java.io.File;
 
 import static java.lang.String.format;
 import static org.eclipse.dataspaceconnector.common.testfixtures.TestUtils.tempDirectory;
-import static org.eclipse.dataspaceconnector.system.tests.utils.TransferSimulationUtils.PROVIDER_ASSET_NAME;
+import static org.eclipse.dataspaceconnector.system.tests.utils.TransferSimulationUtils.PROVIDER_ASSET_FILE;
 
 /**
  * Runs a single iteration of contract negotiation and file transfer, getting settings from
@@ -28,7 +28,7 @@ import static org.eclipse.dataspaceconnector.system.tests.utils.TransferSimulati
  */
 public class FileTransferLocalSimulation extends TransferLocalSimulation {
     public static final String CONSUMER_ASSET_PATH = new File(tempDirectory(), "output.txt").getAbsolutePath();
-    public static final String PROVIDER_ASSET_PATH = format("%s/%s.txt", tempDirectory(), PROVIDER_ASSET_NAME);
+    public static final String PROVIDER_ASSET_PATH = format("%s/%s.txt", tempDirectory(), PROVIDER_ASSET_FILE);
 
     public FileTransferLocalSimulation() {
         super(new FileTransferRequestFactory(CONSUMER_ASSET_PATH));
