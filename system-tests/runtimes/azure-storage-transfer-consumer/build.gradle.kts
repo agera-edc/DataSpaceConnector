25d0ea100fa1a6ab8f2c4adfb9bc118d29035d41
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-04-27 09:26:15 +0200

    Update test with vault for runtime

--- system-tests/runtimes/azure-storage-transfer-consumer/build.gradle.kts
+++ system-tests/runtimes/azure-storage-transfer-consumer/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(project(":data-protocols:ids"))
 
     implementation(project(":extensions:azure:blobstorage"))
+    implementation(project(":extensions:azure:vault"))
 
     api(project(":spi"))
     implementation(project(":common:util"))
