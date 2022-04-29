2fd62a5dd8194bab30e5d0ab594eb00d77462717
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-04-29 11:43:49 +0200

    Updating dependencies

--- samples/04.3-open-telemetry/provider/build.gradle.kts
+++ samples/04.3-open-telemetry/provider/build.gradle.kts
@@ -25,6 +25,23 @@ val azureResourceManagerDataFactory: String by project
 val azureResourceManagerVersion: String by project
 
 dependencies {
+    implementation(project(":core"))
+
+    implementation(project(":extensions:in-memory:assetindex-memory"))
+    implementation(project(":extensions:in-memory:policy-store-memory"))
+    implementation(project(":extensions:in-memory:transfer-store-memory"))
+    implementation(project(":extensions:in-memory:negotiation-store-memory"))
+    implementation(project(":extensions:in-memory:contractdefinition-store-memory"))
+
+    implementation(project(":extensions:api:observability"))
+
+    implementation(project(":extensions:filesystem:configuration-fs"))
+    implementation(project(":extensions:iam:iam-mock"))
+
+    implementation(project(":extensions:api:auth-tokenbased"))
+    implementation(project(":extensions:api:data-management"))
+
+    implementation(project(":data-protocols:ids"))
 
     //implementation(project(":samples:04.0-file-transfer:transfer-file"))
     runtimeOnly(project(":extensions:jdk-logger-monitor"))
