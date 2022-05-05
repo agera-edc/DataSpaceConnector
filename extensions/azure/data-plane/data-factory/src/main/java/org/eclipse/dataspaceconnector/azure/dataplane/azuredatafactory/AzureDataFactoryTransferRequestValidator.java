bbaa7c63c57aa9b5e0fa2b976fc0ab1f4a318fc3
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-05-05 16:53:54 +0200

    Update UTs

--- extensions/azure/data-plane/data-factory/src/main/java/org/eclipse/dataspaceconnector/azure/dataplane/azuredatafactory/AzureDataFactoryTransferRequestValidator.java
+++ extensions/azure/data-plane/data-factory/src/main/java/org/eclipse/dataspaceconnector/azure/dataplane/azuredatafactory/AzureDataFactoryTransferRequestValidator.java
@@ -26,6 +26,7 @@ import static java.lang.String.format;
 import static org.eclipse.dataspaceconnector.azure.blob.core.validator.AzureStorageValidator.validateAccountName;
 import static org.eclipse.dataspaceconnector.azure.blob.core.validator.AzureStorageValidator.validateBlobName;
 import static org.eclipse.dataspaceconnector.azure.blob.core.validator.AzureStorageValidator.validateContainerName;
+import static org.eclipse.dataspaceconnector.azure.blob.core.validator.AzureStorageValidator.validateKeyName;
 
 /**
  * Validator for {@link AzureDataFactoryTransferService}.
@@ -67,7 +68,7 @@ class AzureDataFactoryTransferRequestValidator {
     private void validateProperties(HashMap<String, String> properties) {
         validateAccountName(properties.remove(AzureBlobStoreSchema.ACCOUNT_NAME));
         validateContainerName(properties.remove(AzureBlobStoreSchema.CONTAINER_NAME));
-        properties.remove("keyName");
+        validateKeyName(properties.remove(DataAddress.KEY_NAME));
         properties.keySet().stream().filter(k -> !DataAddress.TYPE.equals(k)).findFirst().ifPresent(k -> {
             throw new IllegalArgumentException(format("Unexpected property %s", k));
         });
