33b8b5e3c69eef1203f2cdbfdd1265df8c0162fd
Alexandre Gattiker <algattik@microsoft.com>
2022-06-21 11:09:04 +0200

    Update VerifiableCredentialFactoryTest.java

--- extensions/iam/decentralized-identity/identity-did-crypto/src/test/java/org/eclipse/dataspaceconnector/iam/did/crypto/credentials/VerifiableCredentialFactoryTest.java
+++ extensions/iam/decentralized-identity/identity-did-crypto/src/test/java/org/eclipse/dataspaceconnector/iam/did/crypto/credentials/VerifiableCredentialFactoryTest.java
@@ -17,15 +17,24 @@ package org.eclipse.dataspaceconnector.iam.did.crypto.credentials;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import org.eclipse.dataspaceconnector.iam.did.crypto.key.EcPrivateKeyWrapper;
 import org.eclipse.dataspaceconnector.iam.did.crypto.key.EcPublicKeyWrapper;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Clock;
 import java.time.Instant;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 import static java.time.ZoneOffset.UTC;
 import static java.time.temporal.ChronoUnit.MINUTES;
@@ -47,6 +56,7 @@ class VerifiableCredentialFactoryTest {
     }
 
     @Test
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     void createVerifiableCredential() throws Exception {
         var vc = VerifiableCredentialFactory.create(privateKey, "test-connector", "test-audience", clock);
 
@@ -54,7 +64,7 @@ class VerifiableCredentialFactoryTest {
         assertThat(vc.getJWTClaimsSet().getIssuer()).isEqualTo("test-connector");
         assertThat(vc.getJWTClaimsSet().getSubject()).isEqualTo("verifiable-credential");
         assertThat(vc.getJWTClaimsSet().getAudience()).containsExactly("test-audience");
-        assertThat(vc.getJWTClaimsSet().getJWTID()).satisfies(c -> UUID.fromString(c));
+        assertThat(vc.getJWTClaimsSet().getJWTID()).satisfies(UUID::fromString);
         assertThat(vc.getJWTClaimsSet().getExpirationTime()).isEqualTo(now.plus(10, MINUTES).truncatedTo(SECONDS));
     }
 
@@ -73,16 +83,44 @@ class VerifiableCredentialFactoryTest {
         assertThat(deserialized.getPayload().toString()).isEqualTo(vc.getPayload().toString());
     }
 
-    @Test
-    void verifyJwt() throws Exception {
+    @ParameterizedTest(name = "{2}")
+    @MethodSource("verifyJwtArgs")
+    void verifyJwt(UnaryOperator<JWTClaimsSet.Builder> builderOperator, boolean expectSuccess, String ignoredName) throws Exception {
         var vc = VerifiableCredentialFactory.create(privateKey, "test-connector", "test-audience", clock);
-        String jwtString = vc.serialize();
 
-        //deserialize
-        var jwt = SignedJWT.parse(jwtString);
+        JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder(vc.getJWTClaimsSet());
 
-        assertThat(VerifiableCredentialFactory.verify(jwt, publicKey, "test-audience")).isTrue();
+        var jwt = new SignedJWT(
+                vc.getHeader(),
+                builderOperator.apply(builder).build());
+        jwt.sign(privateKey.signer());
 
+        assertThat(VerifiableCredentialFactory.verify(jwt, publicKey, "test-audience")).isEqualTo(expectSuccess);
+    }
+
+    public static Stream<Arguments> verifyJwtArgs() {
+        return Stream.of(
+                jwtCase(b -> b, true, "valid token"),
+                jwtCase(b -> b.audience(List.of()), false, "empty audience"),
+                jwtCase(b -> b.audience(List.of("https://otherserver.com")), false, "wrong audience"),
+                jwtCase(b -> b.audience(List.of("test-audience")), true, "expected audience"), // sanity check
+                jwtCase(b -> b.subject(null), false, "empty subject"),
+                jwtCase(b -> b.subject("other-subject"), false, "wrong subject"),
+                jwtCase(b -> b.subject("verifiable-credential"), true, "expected subject"), // sanity check
+                jwtCase(b -> b.issuer(null), false, "empty issuer"),
+                jwtCase(b -> b.issuer("other-issuer"), true, "other issuer"),
+                jwtCase(b -> b.expirationTime(null), false, "empty expiration"),
+                // Nimbus library allows (by default) max 60 seconds of expiration date clock skew
+                jwtCase(b -> b.expirationTime(Date.from(Instant.now().minus(61, SECONDS))), false, "past expiration beyond max skew"),
+                jwtCase(b -> b.expirationTime(Date.from(Instant.now().minus(1, SECONDS))), true, "past expiration within max skew"),
+                jwtCase(b -> b.expirationTime(Date.from(Instant.now().plus(1, MINUTES))), true, "future expiration"),
+                jwtCase(b -> b.claim("foo", "bar"), true, "additional claim")
+        );
+    }
+
+    @NotNull
+    private static Arguments jwtCase(UnaryOperator<JWTClaimsSet.Builder> builderOperator, boolean expectSuccess, String name) {
+        return Arguments.of(builderOperator, expectSuccess, name);
     }
 
     private JWK getJwk(String resourceName) throws JOSEException {
