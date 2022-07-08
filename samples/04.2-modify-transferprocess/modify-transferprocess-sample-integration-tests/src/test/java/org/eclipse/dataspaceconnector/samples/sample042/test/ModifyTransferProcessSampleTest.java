3e59ca501dc26d443e28cafe7d9fc65e1bcbfd98
Peter Kirchner <peter.kirchner@microsoft.com>
2022-07-08 13:55:10 +0200

    samples: add method to retrieve transfer process

--- samples/04.2-modify-transferprocess/modify-transferprocess-sample-integration-tests/src/test/java/org/eclipse/dataspaceconnector/samples/sample042/test/ModifyTransferProcessSampleTest.java
+++ samples/04.2-modify-transferprocess/modify-transferprocess-sample-integration-tests/src/test/java/org/eclipse/dataspaceconnector/samples/sample042/test/ModifyTransferProcessSampleTest.java
@@ -14,6 +14,9 @@
 
 package org.eclipse.dataspaceconnector.samples.sample042.test;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.apache.http.HttpStatus;
 import org.eclipse.dataspaceconnector.common.util.junit.annotations.EndToEndTest;
 import org.eclipse.dataspaceconnector.junit.extensions.EdcRuntimeExtension;
 import org.eclipse.dataspaceconnector.junit.testfixtures.TestUtils;
@@ -22,12 +25,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.not;
 
 @EndToEndTest
 public class ModifyTransferProcessSampleTest {
 
     static final String CONSUMER_CONFIG_PROPERTIES_FILE_PATH = "samples/04.2-modify-transferprocess/consumer/config.properties";
+    static final String TRANSFER_PROCESS_URI = "http://localhost:9192/api/v1/data/transferprocess";
+    static final String API_KEY_HEADER_KEY = "X-Api-Key";
+    static final String API_KEY_HEADER_VALUE = "password";
     @RegisterExtension
     static EdcRuntimeExtension consumer = new EdcRuntimeExtension(
             ":samples:04.2-modify-transferprocess:consumer",
@@ -36,6 +48,8 @@ public class ModifyTransferProcessSampleTest {
                     "edc.fs.config", getFileFromRelativePath(CONSUMER_CONFIG_PROPERTIES_FILE_PATH).getAbsolutePath()
             )
     );
+    static final Duration DURATION = Duration.ofSeconds(15);
+    static final Duration POLL_INTERVAL = Duration.ofMillis(500);
 
     /**
      * Resolves a {@link File} instance from a relative path.
@@ -45,11 +59,32 @@ public class ModifyTransferProcessSampleTest {
         return new File(TestUtils.findBuildRoot(), relativePath);
     }
 
+
     /**
      * Let run the consumer EDC.
      */
     @Test
-    void runSample() throws InterruptedException {
-        Thread.sleep(10000);
+    void runSample() {
+        getTransferProcesses();
+    }
+
+    void getTransferProcesses() {
+        AtomicReference<JsonPath> result = new AtomicReference<>();
+
+        await().atMost(DURATION).pollInterval(POLL_INTERVAL)
+                .untilAsserted(() ->
+                        result.set(RestAssured
+                                .given()
+                                    .headers(API_KEY_HEADER_KEY, API_KEY_HEADER_VALUE)
+                                .when()
+                                    .get(TRANSFER_PROCESS_URI)
+                                .then()
+                                    .statusCode(HttpStatus.SC_OK)
+                                    .body("id", not(emptyString()))
+                                    .extract()
+                                    .jsonPath())
+        );
+
+        System.out.print(result);
     }
 }
